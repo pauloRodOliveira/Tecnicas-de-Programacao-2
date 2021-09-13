e33b82e8ca138d21276ccc122e5f87b4c953686d
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2021-09-13 17:55:17 -0300

    adicionando pasta de teste
    coloquei esta pasta para poder aprender a utilizar o intelliJ .

---
+++ (DS201) TÉCNICAS DE PROGRAMACÃO II/teste/src/classes/Teste.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Teste {
+        private byte d = 1;
+
+        public Teste(byte d){
+            this.d = d;
+        }
+
+        public int setDoubleValueOf(byte n){
+            return n * 2;
+        }
+}
---
+++ (DS201) TÉCNICAS DE PROGRAMACÃO II/teste/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.io.*;
+import classes.Teste;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Digite um número que queira dobrar o valor: ");
+
+            byte n = Byte.parseByte(teclado.readLine());
+
+            Teste numeroDigitado = new Teste(n);
+            System.out.println(numeroDigitado.setDoubleValueOf(n));
+
+        }catch (Exception e){
+        }
+    }
+}
