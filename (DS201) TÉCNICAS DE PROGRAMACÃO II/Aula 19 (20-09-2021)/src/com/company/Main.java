c772e7003f15936e34ab690688e187801f32ac8d
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2021-09-20 22:50:18 -0300

    Aula 20-09-2021 sobre static

---
+++ (DS201) TÉCNICAS DE PROGRAMACÃO II/Aula 19 (20-09-2021)/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.lang.System;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s = new String ("123");
+        int i = Integer.parseInt(s);
+        Integer.parseInt(s);
+        // ^ Chamante.^ Método
+        //Chamante pode ser uma Classe ou um Objeto.
+        var out = System.out;
+        out.println(i);
+    }
+}
