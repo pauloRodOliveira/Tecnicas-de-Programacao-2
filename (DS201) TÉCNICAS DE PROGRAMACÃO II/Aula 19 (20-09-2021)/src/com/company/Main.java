43dd69c81312b79d2510c792f867f2946d95f94f
Paulo Rodrigues <80362527+pauloRodOliveira@users.noreply.github.com>
2021-09-21 15:28:10 -0300

    alteração

--- (DS201) TÉCNICAS DE PROGRAMACÃO II/Aula 19 (20-09-2021)/src/com/company/Main.java
+++ (DS201) TÉCNICAS DE PROGRAMACÃO II/Aula 19 (20-09-2021)/src/com/company/Main.java
@@ -9,6 +9,10 @@ public class Main {
         Integer.parseInt(s);
         // ^ Chamante.^ Método
         //Chamante pode ser uma Classe ou um Objeto.
+        /*Quando o método ser static, ele não vai poder ter um Objeto como chamante, somente uma Classe.
+        * Por exemplo, o método Integer.parseInt(s); este método é estático, portanto não é necessário criar um
+        * Objeto da Classe Wrapper Integer para usar este método.
+        * */
         var out = System.out;
         out.println(i);
     }
